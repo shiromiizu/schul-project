4f605caae1cf552b0c834da2ce5e5932509a301f
b3-fabi <fabiv3003@gmail.com>
2025-12-15 14:14:38 +0100

    wip

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import StudentDashboardComponent from "@/components/studentDashboardComponent";
+import { NextPage } from "next";
+
+const DashboardPage: NextPage = () => {
+  return (
+    <StudentDashboardComponent/>
+    );
+}
+
+export default DashboardPage;
