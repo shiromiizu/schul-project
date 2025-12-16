0209c9a6e56908975579fbc1d724108b63f2bc24
bogdan <b.glu@icloud.com>
2025-12-16 09:34:28 +0100

    ✨ - removed unused svgs - added logo

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server"
 import { UserMenu } from "./user-menu"
 import { ModeToggle } from "./mode-toggle"
 import Link from "next/link"
+import Image from 'next/image';
 
 export async function Header() {
   const supabase = await createClient()
@@ -21,10 +22,8 @@ export async function Header() {
     <header className="w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 shadow-sm">
       <div className="w-full flex h-14 items-center justify-between px-4">
         <div className="mr-4 flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
-            <span className="font-bold sm:inline-block">
-              b3echo
-            </span>
+          <Link href="/" className="flex hover:opacity-80 transition-opacity">
+            <Image src={'/b3echo-grafitti.svg'} alt={'B3Echo'} width={140} height={0} />
           </Link>
         </div>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
@@ -35,5 +34,5 @@ export async function Header() {
         </div>
       </div>
     </header>
-  )
+  );
 }
